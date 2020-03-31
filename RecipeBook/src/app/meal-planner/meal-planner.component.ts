3721ac6d6850094e3cc868aabe1c81bdb7aca1eb
Gustavo Adami <gustavo_adami@hotmail.com>
2020-03-31 09:47:24 -0400

    Meal planner v1-NoDB

--- RecipeBook/src/app/meal-planner/meal-planner.component.ts
+++ RecipeBook/src/app/meal-planner/meal-planner.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {CdkDrag, CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { CdkDrag, CdkDragDrop, moveItemInArray, transferArrayItem, copyArrayItem } from '@angular/cdk/drag-drop';
 import { MatTableDataSource } from '@angular/material';
 
 import { RecipesListService } from '../recipes-list/recipes-list.service';
+import { compileNgModule } from '@angular/compiler';
 
 
 @Component({
@@ -15,120 +16,107 @@ export class MealPlannerComponent implements OnInit {
   title = "Meal Planner";
   dataSource = new MatTableDataSource<any>();
 
-
   constructor(public recipeService: RecipesListService) { }
 
   ngOnInit() {
     this.refresh();
   }
 
-  async refresh(){
+  async refresh() {
     const data = await this.recipeService.getRecipes();
     this.dataSource.data = data;
+    this.putToList();
   }
+
+  putToList() {
+    this.listOfRecipes = [];
+
+    this.dataSource.data.forEach(element => {
+      // console.log(element.recipeTitle);
+      this.listOfRecipes.push(element.recipeTitle);
+
+    });
+  }
+
+  listOfRecipes = [];
 
   mealsTypes = [
     'Breakfast',
     'Lunch',
+    'Dessert',
     'Dinner',
     'Supper'
   ];
 
   mondayRecipes = [
-    // 'Get to work',
-    // 'Pick up groceries',
-    // 'Go home',
-    // 'Fall asleep'
-    // '','','',''
+    '', '', '', '', ''
   ];
 
   tuesdayRecipes = [
-    // 'Get up',
-    // 'Brush teeth',
-    // 'Take a shower',
-    // 'Check e-mail',
+    '', '', '', '', ''
   ];
 
   wednesdayRecipes = [
-    // 'Get up',
-    // 'Brush teeth',
-    // 'Take a shower',
-    // 'Check e-mail',
+    '', '', '', '', ''
   ];
 
   thursdayRecipes = [
-    // 'Get up',
-    // 'Brush teeth',
-    // 'Take a shower',
-    // 'Check e-mail',
+    '', '', '', '', ''
   ];
 
   fridayRecipes = [
-    // 'Get up',
-    // 'Brush teeth',
-    // 'Take a shower',
-    // 'Check e-mail',
+    '', '', '', '', ''
   ];
 
   saturdayRecipes = [
-    // 'Get up',
-    // 'Brush teeth',
-    // 'Take a shower',
-    // 'Check e-mail',
-
+    '', '', '', '', ''
   ];
 
   sundayRecipes = [
-    // 'Get up',
-    // 'Brush teeth',
-    // 'Take a shower',
-    // 'Check e-mail',
+    '', '', '', '', ''
   ];
 
   drop(event: CdkDragDrop<string[]>) {
 
     if (event.previousContainer === event.container) {
+      console.log('Same container');
+
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
-      // if(event.container.data[event.currentIndex] == ''){
-      //   console.log('REPLACING DATA empty' );
-      //   // delete event.container.data[event.currentIndex];
-
-
-      //   transferArrayItem(event.previousContainer.data,
-      //     event.container.data,
-      //     event.previousIndex,
-      //     event.currentIndex);
-
-      //   event.container.data.splice(event.currentIndex, 1);
-
-
-      //     return true;
-
-      // }
-
-      if (event.container.data.length >= 4) {
-        alert("Impossible to add Recipe to that day. Your meal planner is full");
+      console.log('Other container');
+      if (event.previousContainer.data[event.previousIndex] == '') {
         return false;
       }
 
+      if (event.container.data[event.currentIndex] === undefined) {
+        copyArrayItem(event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex);
 
+        event.container.data.splice(event.currentIndex - 1, 1);
 
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+        event.previousContainer.data[event.previousIndex] = '';
 
-                        this.refresh();
+      } else if (event.container.data[event.currentIndex].length >= 0) {
+        console.log(`REPLACING DATA - ${event.container.data[event.currentIndex]} at index ${event.currentIndex}`);
+
+        copyArrayItem(event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex);
+
+        event.container.data.splice(event.currentIndex + 1, 1);
+        event.previousContainer.data[event.previousIndex] = '';
+      }
     }
+
+    this.refresh();
 
   }
 
-  removeItem(event, i){
-    console.log('DELTE)');
-    console.log('DATAA ' +  event)
-        event.container.data.splice(i, 1);
-
+  removeItem(array, i) {
+    array[i] = '';
   }
 
 }
--- RecipeBook/src/app/recipes-list/recipes-list.service.ts
+++ RecipeBook/src/app/recipes-list/recipes-list.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { OktaAuthService } from '@okta/okta-angular';
 import { Recipe } from './recipe';
 
-const baseUrl = 'http://192.168.0.25:4201';
+const baseUrl = 'http://localhost:4201';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +16,7 @@ export class RecipesListService {
 
 	private async request(method: string, url: string, data?: any) {
 		const token = await this.oktaAuth.getAccessToken();
-    console.log('request ' + JSON.stringify(data));
+    // console.log('request ' + JSON.stringify(data));
 
 		const result = this.http.request(method, url, {
       body: data,
